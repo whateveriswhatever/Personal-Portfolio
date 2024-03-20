58984e8be1f302b7c466b18b21ee50104b532267
Table Computer <banvietthenaocungdc@gmail.com>
2024-03-20 13:04:13 +0700

    03/20/2024 update

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -3,7 +3,7 @@ import PersonalLogo from "./{components}/PersonalLogo";
 const About: React.FC = () => {
   return (
     <div className="phone:max-w-[86%] tablet:max-w-[21.5em] laptop:max-w-[21.5em] desktop:max-w-[21.5em] tablet:mt-0 tablet:mb-0 tablet:ml-auto tablet:mr-auto desktop:mt-0 desktop:mb-0 desktop:ml-auto desktop:mr-auto laptop:mt-0 laptop:mb-0 laptop:ml-auto laptop:mr-auto">
-      <div className="" id="logo">
+      <div className="float-right" id="logo">
         <PersonalLogo />
       </div>
     </div>
--- src/app/About/{components}/PersonalLogo.tsx
+++ src/app/About/{components}/PersonalLogo.tsx
@@ -4,11 +4,29 @@ import { useState } from "react";
 
 const PersonalLogo = () => {
   const [linkContent, setLinkContent] = useState("DL");
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="float-right">
-      <div className="">
-        <a href="#">{linkContent}</a>
-      </div>
+    <div className="z-[999] fixed top-[10%]">
+      <a
+        href="/work"
+        className="text-neutral-50 cursor-pointer no-underline hover:animate-[slow-move] hover:duration-[0.4s]"
+        onMouseEnter={() => {
+          setLinkContent("DL's works");
+          setIsHovered(true);
+        }}
+        onMouseLeave={() => {
+          setLinkContent("DL");
+          setIsHovered(false);
+        }}
+        // style={{ marginLeft: isHovered ? "-1.2rem" : "0" }}
+        style={{
+          opacity: isHovered ? "999" : "1",
+          transform: isHovered ? "translateY(10vh)" : "translateY(0vh)",
+          marginLeft: isHovered ? "-1.2rem" : "0",
+        }}
+      >
+        {linkContent}
+      </a>
     </div>
   );
 };
