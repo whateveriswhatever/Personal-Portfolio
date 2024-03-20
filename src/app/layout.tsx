9b62fb6ced7e423a39b858f79c43be21a3444aa3
Table Computer <banvietthenaocungdc@gmail.com>
2024-03-20 13:00:27 +0700

    Modiying the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div
-          className="h-screen max-h-screen website-background-black text-white"
-          style={{ backgroundColor: "#0c0c0c" }}
+          className="h-screen max-h-screen website-background-black text-white p-0 m-0"
+          style={{ backgroundColor: "#0c0c0c", fontSize: "calc(1em + 1vw)" }}
         >
           {children}
         </div>
