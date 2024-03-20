94391f89ad077259ca78d4adfe379e61d0483556
Table Computer <banvietthenaocungdc@gmail.com>
2024-03-20 16:34:55 +0700

    Daily update at 4:34PM on 03/20/2024

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -5,7 +5,10 @@ import PageContents from "./{components}/PageContents";
 const About: React.FC = () => {
   return (
     <div className="phone:max-w-[86%] tablet:max-w-[21.5em] laptop:max-w-[21.5em] desktop:max-w-[21.5em] tablet:mt-0 tablet:mb-0 tablet:ml-auto tablet:mr-auto desktop:mt-0 desktop:mb-0 desktop:ml-auto desktop:mr-auto laptop:mt-0 laptop:mb-0 laptop:ml-auto laptop:mr-auto phone:mb-0 phone:mt-0 phone:ml-auto phone:mr-auto">
-      <div className="float-right phone:mr-8" id="logo">
+      <div
+        className="float-right phone:mr-[2rem] tablet:mr-[-2.4rem]"
+        id="logo"
+      >
         <PersonalLogo />
       </div>
 
--- src/app/About/{components}/PageContents.tsx
+++ src/app/About/{components}/PageContents.tsx
@@ -15,13 +15,13 @@ const PageContents = () => {
       </>
 
       <>
-        <Paragraph context="lorem isump" />
-        <Paragraph context="lorem isump" />
-        <Paragraph context="lorem isump" />
+        <Paragraph context="As a sophomore student at Vietnam National University, specializing in Informatics and Computer Engineering, I am deeply passionate about computer science, particularly in the field of software development. With a keen interest in web development, I have dedicated myself to mastering various technologies and frameworks over the past year." />
+        <Paragraph context="I am a dedicated and driven learner, constantly seeking to expand my knowledge and skill set in web development. Through self-directed study, coursework, and hands-on projects, I have cultivated a strong foundation in front-end and back-end development, as well as proficiency in popular web development tools and technologies." />
+        <Paragraph context="Creative Problem Solver: With a natural aptitude for problem-solving and a creative mindset, I thrive in tackling challenges and finding innovative solutions. Whether it's crafting intuitive user interfaces, optimizing website performance, or debugging complex issues, I approach every problem with curiosity and determination." />
 
-        <Paragraph context="lorem isump" />
-        <Paragraph context="lorem isump" />
-        <Paragraph context="lorem isump" />
+        <Paragraph context="Collaborative Team Player: I value collaboration and teamwork, recognizing the importance of effective communication and cooperation in achieving shared goals. I enjoy working collaboratively with colleagues and peers, contributing my skills and insights while also learning from others' perspectives and experiences." />
+        <Paragraph context="Continuous Improvement Advocate: Committed to personal and professional growth, I embrace a growth mindset and actively seek feedback to further refine my skills and abilities. I am constantly exploring new technologies, attending workshops and seminars, and engaging in online communities to stay updated on the latest trends and best practices in web development." />
+        <Paragraph context="Future-Focused Professional: As I progress in my academic and professional journey, I am excited to continue honing my expertise in web development and pursuing opportunities to make meaningful contributions to the field. With a solid foundation in computer science principles and a passion for creating impactful digital experiences, I am poised to thrive in the ever-evolving landscape of software development." />
 
         <ResumeLink />
       </>
